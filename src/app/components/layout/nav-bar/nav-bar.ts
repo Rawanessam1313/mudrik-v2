456d2b959c84b22829e56931565214477473c4e8
Rawanessam1313 <rawan11essam@gmail.com>
2026-06-18 04:53:15 +0200

    add navbar component

---
+++ src/app/components/layout/nav-bar/nav-bar.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.css',
+})
+export class NavBar {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
