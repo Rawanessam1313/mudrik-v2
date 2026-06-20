662ad68e11d791c7a6ecc082fbddf87588b1c3d2
HodaSaraaya <hoda.saraya1999@gmail.com>
2026-06-20 17:46:07 +0200

    Implement login page component; scaffold register page (not yet implemented)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBar } from './components/layout/nav-bar/nav-bar';
+//import { NavBar } from './components/layout/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet ,NavBar],
+  //imports: [RouterOutlet ,NavBar]
+  imports: [RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page';
+
+export const AUTH_ROUTES: Routes = [
+  { path: 'login', component: LoginPageComponent }
+];
