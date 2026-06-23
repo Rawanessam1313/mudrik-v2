bc6b03075d2ce5612f5e4c73ac3354f79aefeb1d
Rawanessam1313 <rawan11essam@gmail.com>
2026-06-24 00:38:18 +0200

    fix routes

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page';
 
 export const AUTH_ROUTES: Routes = [
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login-page', component: LoginPageComponent }
 ];
