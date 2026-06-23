1dd56ed39eb026a206e3b010a6fe26a180a7c537
HodaSaraaya <hoda.saraya1999@gmail.com>
2026-06-23 13:02:03 +0200

    Implement register page component and add register route

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page';
+import { RegisterPageComponent } from './components/register-page/register-page';
 
 export const AUTH_ROUTES: Routes = [
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent }
 ];
